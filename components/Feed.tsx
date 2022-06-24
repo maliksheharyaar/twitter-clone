f3bf84f25df57746c116261d6e0fcf7feff596f1
Sheharyaar <38083700+maliksheharyaar@users.noreply.github.com>
2022-06-24 01:07:28 -0400

    [TC-1]: Initial setup with sanity and tweet display

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { RefreshIcon } from '@heroicons/react/outline';
-
-const Feed = () => {
-  return (
-    <div>
-        <div className="flex items-center justify-between">
-            <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-            <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter 
-            transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
-        </div>
-    </div>
-  )
+import React from "react";
+import { RefreshIcon } from "@heroicons/react/outline";
+import TweetBox from "./TweetBox";
+import { Tweet } from "../typings";
+import TweetComponent from '../components/Tweet';
+interface Props { 
+  tweets: Tweet[]
 }
 
-export default Feed
+const Feed = ({ tweets } : Props) => {
+  return (
+    <div className="col-span-7 lg:col-span-5 border-x">
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon
+          className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter 
+            transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+        />
+      </div>
+
+      <div>
+        <TweetBox />
+      </div>
+
+      { /* Feed */}
+      <div>
+        {tweets.map(tweet => (
+          <TweetComponent key={tweet._id} tweet={tweet} />
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Feed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,29 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props { 
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets);
+
   return (
-    <div className="">
+    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
         <title>Twitter Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
-        <Feed />
-
-        {/* Widgets */}
+        <Feed tweets={tweets} />
+        <Widgets />
 
       </main>
 
@@ -25,3 +32,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+
+  return { 
+    props: {
+      tweets,
+    }
+  }
+}
